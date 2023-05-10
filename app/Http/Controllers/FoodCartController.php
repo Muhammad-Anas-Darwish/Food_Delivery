afd899a308ad0363c8ec6985a6d6e4bc323febd2
Muhammad-Anas-Darwish <muhammadanasdarwish1@gmail.com>
2023-05-10 15:31:17 +0300

    add model, factory, seed, controller files

---
+++ app/Http/Controllers/FoodCartController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FoodCart;
+use Illuminate\Http\Request;
+
+class FoodCartController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FoodCart $foodCart)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(FoodCart $foodCart)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, FoodCart $foodCart)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FoodCart $foodCart)
+    {
+        //
+    }
+}
