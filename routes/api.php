5b2cd9703465287bb08e0a1c50bc27b1b28b2c77
Muhammad-Anas-Darwish <muhammadanasdarwish1@gmail.com>
2023-05-13 16:23:02 +0300

    add register and login

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('login', [UserController::class, 'login']);
+Route::post('register', [UserController::class, 'register']);
+Route::group(['middleware' => 'auth:api'], function(){
+Route::post('details', [UserController::class, 'details']);
+});
+
+// Route::post('login', 'API\UserController@login');
+// Route::post('register', 'API\UserController@register');
+// Route::group(['middleware' => 'auth:api'], function(){
+// Route::post('details', 'API\UserController@details');
+// });
