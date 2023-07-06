71d691c0dfaa4f2ffda407fcd99e452cc4eee794
Muhammad-Anas-Darwish <muhammadanasdarwish1@gmail.com>
2023-07-06 15:34:13 +0300

    Update slug when update category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,6 +6,7 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class CategoryController extends Controller
 {
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,41 +9,22 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Category extends Model
 {
     use HasFactory;
+    use Sluggable;
 
     protected $fillable = ['title', 'slug'];
 
     /**
-     * Boot the model.
-     */
-    protected static function boot()
-    {
-        parent::boot();
-
-        static::created(function ($category) {
-            $category->slug = $category->createSlug($category->title);
-            $category->save();
-        });
-    }
-
-    /**
-     * Write code on Method
+     * Return the sluggable configuration array for this model.
      *
-     * @return response()
+     * @return array
      */
-    private function createSlug($title)
+    public function sluggable(): array
     {
-        if (static::whereSlug($slug = Str::slug($title))->exists()) {
-            $max = static::whereTitle($title)->latest('id')->skip(1)->value('slug');
-
-            if (is_numeric($max[-1])) {
-                return preg_replace_callback('/(\d+)$/', function ($mathces) {
-                    return $mathces[1] + 1;
-                }, $max);
-            }
-
-            return "{$slug}-2";
-        }
-
-        return $slug;
+        return [
+            'slug' => [
+                'source' => 'title',
+                'onUpdate' => true,
+            ]
+        ];
     }
 }
